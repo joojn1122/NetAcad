d66c75233a32b805b29a1e47327359db44a6e777
joojn <85359001+joojn1122@users.noreply.github.com>
2022-06-14 20:51:32 +0200

    Update updater.js

--- server/assets/updater.js
+++ server/assets/updater.js
@@ -5,7 +5,7 @@ let mainLink = "https://raw.githack.com/joojn1122/netacad/main/server"
 
 console.log("Looking for updates..")
 
-const json = JSON.parse(fs.readFileSync(__dirname + "\\..\\info.json"), {encoding:'utf8', flag:'r'})
+const json = JSON.parse(fs.readFileSync(__dirname + "//..//info.json"), {encoding:'utf8', flag:'r'})
 const version = json['version']
 
 const newJson = fetch(`${mainLink}/info.json`).json()
@@ -14,26 +14,28 @@ const newVersion = newJson['version']
 
 if(newVersion !== version)
 {
-	console.log("Update found, updating..")
-	try
-	{
-		let newServerJS = fetch(`${mainLink}/server.js`).text()
-		let newUtilJS = fetch(`${mainLink}/util.js`).text()
+        console.log("Update found, updating..")
+        try
+        {
+                let newServerJS = fetch(`${mainLink}/assets/server.js`).text()
+                let newUtilJS = fetch(`${mainLink}/assets/util.js`).text()
+                let newGoogleJS = fetch(`${mainLink}/assets/google.js`).text()
 
-		fs.writeFileSync("server.js", newServerJS, "utf8")
-		fs.writeFileSync("util.js", newUtilJS, "utf8")
+                fs.writeFileSync("server.js", newServerJS, "utf8")
+                fs.writeFileSync("util.js", newUtilJS, "utf8")
+                fs.writeFileSync("google.js", newGoogleJS, "utf8")
 
-		fs.writeFileSync(__dirname + "\\..\\info.json", JSON.stringify(newJson, null, 4), "utf8")
-	}	
-	catch(e)
-	{
-		console.log("Error happened.. " + e)
-		process.exit()
-	}
+                fs.writeFileSync(__dirname + "//..//info.json", JSON.stringify(newJson, null, 4), "utf8")
+        }
+        catch(e)
+        {
+                console.log("Error happened.. " + e)
+                process.exit()
+        }
 }
 else
 {
-	console.log("Up to date!");
+        console.log("Up to date!");
 }
 
 const server = require("./server")
