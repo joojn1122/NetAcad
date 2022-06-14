dccfa5661093a5d4aa4139e76cdb0806ff67f0d6
joojn <85359001+joojn1122@users.noreply.github.com>
2022-06-14 21:25:21 +0200

    Update inject.js

--- chrome_extension/inject.js
+++ chrome_extension/inject.js
@@ -1,3 +1,4 @@
 let script = document.createElement("script")
-script.src="https://raw.githack.com/joojn1122/NetAcad/main/chrome_extension/solver.js"
+//script.src="https://raw.githack.com/joojn1122/NetAcad/main/chrome_extension/solver.js"
+script.src="https://rawcdn.githack.com/joojn1122/NetAcad/05f1abe2f83dda4c3cecbf61889dc4537800e369/chrome_extension/solver.js"
 document.head.appendChild(script)
